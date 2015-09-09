01e860452cce62b636eca26accbde45a20fd4812
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2015-09-10 02:41:37 +0900

    3rd commit. add static and refact by generic

--- works/views.py
+++ works/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from django.views import generic
 #from django.template import RequestContext, loader
 
 from .models import Works
 
 # Create your views here.
+#トップ画面
+class IndexView(generic.ListView):
+	template_name="works/index.html"
+	context_object_name="new_archieve_list"
 
-#top画面
-def index(request):
-	new_archieve_list = Works.objects.order_by('work_start')[:10]
-	context = {'new_archieve_list' : new_archieve_list}
-	return render(request, 'works/index.html', context)
-	#template = loader.get_template('works/index.html')
-	#context = RequestContext(request, {
-	#	'new_archieve_list' : new_archieve_list,
-	#	})
-	#output = ', '.join([p.title for p in new_archieve_list])
-	return HttpResponse(template.render(context))
+	def get_queryset(self):
+		return Works.objects.order_by('work_start')[:10]
 
-def detail(request, works_id):
-	work = get_object_or_404(Works, pk=works_id)
-	return render(request, 'works/detail.html', {'work': work})
-	
+#バイト詳細情報画面
+class DetailView(generic.DetailView):
+	model = Works
+	template_name = "works/detail.html"
