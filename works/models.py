acb731e3b203e6869497fe0ef819ff554f1e1b60
kazuku higashiguchi <higashiguchi_kazuki@yahoo.co.jp>
2015-09-10 19:23:38 +0900

    night

--- works/models.py
+++ works/models.py
@@ -5,21 +5,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Works(models.Model):
-	IMADE = "IM"
-	TANABE = "TN"
+
 	CANPUS_CHOICE = (
-		(IMADE , u'今出川'),
-		(TANABE , u'京田辺'),
+		(0 , u'今出川'),
+		(1 , u'京田辺'),
 	)
-	HIKEN = "Hi"
-	Seikyo = "Se"
-	PC = "Pc"
-	Hozyo = "Ho"
+	
 	CATEGORY_CHOICE = (
-		(HIKEN, u'被験者バイト'),
-		(Seikyo, u'生協バイト'),
-		(PC, u'PCバイト'),
-		(Hozyo, u'補助バイト'),
+		(0, u'被験者バイト'),
+		(1, u'生協バイト'),
+		(2, u'PCバイト'),
+		(3, u'授業補助'),
+		(4, u"イベント"),
 	)
 	
 	title = models.CharField(max_length=30)
@@ -28,13 +25,13 @@ class Works(models.Model):
 	pub_mail = models.EmailField(max_length=254)
 	content = models.TextField()
 	condition = models.CharField(max_length=100)
-	canvas = models.CharField(max_length=2, choices=CANPUS_CHOICE)
+	canvas = models.IntegerField(choices=CANPUS_CHOICE)
 	location = models.CharField(max_length=100)
 	work_period = models.DateField()
 	dead_line = models.DateField()
 	can_time = models.DateField()
 	pay = models.CharField(max_length=20)
-	category = models.CharField(max_length=2, choices=CATEGORY_CHOICE)
+	category = models.IntegerField(choices=CATEGORY_CHOICE)
 	
 	#top_image = models.FilePathField(upload_to=None, max_length=100)
 
